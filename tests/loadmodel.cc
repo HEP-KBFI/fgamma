7fadbf5045203ee54aa7df1170069b4920f362bf
Morten Piibeleht <morten.piibeleht@cern.ch>
2014-02-27 17:04:52 +0200

    Add a test program that loads the example geometry.

---
+++ tests/loadmodel.cc
@@ -0,0 +1,18 @@
+#include "../src/DetectorConstruction.hh"
+#include <G4NistManager.hh>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	G4Element * e = G4NistManager::Instance()->FindOrBuildElement("O");
+	cout << *e << endl;
+
+	G4IsotopeVector * gv = (*e).GetIsotopeVector();
+	for(G4IsotopeVector::iterator it = gv->begin(); it!=gv->end(); ++it) {
+		G4Isotope * iso = *it;
+		cout << iso << endl;
+	}
+
+	cout << "---------------------- DetectorConstruction ----------------------" << endl;
+	DetectorConstruction("model.yml");
+}
