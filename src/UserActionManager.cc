d2c52b9bbffc585ed3d5fc777a155ff4ed9a22a8
Morten Piibeleht <morten.piibeleht@cern.ch>
2014-03-14 15:45:29 +0200

    Reorganize the UserActionManager.
    This is to reduce the unnecessary complexity the previous system
    introduced. All common variables are now encaptulated in the
    UserActionManager::CommonVariables structure, which is passed on
    to the Geant4 user action classes.

--- src/UserActionManager.cc
+++ src/UserActionManager.cc
@@ -1,5 +1,13 @@
 #include "UserActionManager.hh"
 
+#include <G4UserSteppingAction.hh>
+#include <G4UserEventAction.hh>
+#include <G4UserStackingAction.hh>
+#include <G4UserTrackingAction.hh>
+#include <G4VUserTrackInformation.hh>
+
+#include <G4Event.hh>
+#include <G4Track.hh>
 #include <G4Gamma.hh>
 
 using namespace CLHEP;
@@ -9,65 +17,63 @@ using namespace CLHEP;
 // ---------------------------------------------------------------------
 
 class UAIUserSteppingAction : public G4UserSteppingAction {
-	UserActionsInterface * pUAI;
+	UserActionManager::CommonVariables &pUAI;
 	public:
-		UAIUserSteppingAction(UserActionsInterface * uai) : pUAI(uai) {}
-		void UserSteppingAction(const G4Step * step) {pUAI->step(step);}
+		UAIUserSteppingAction(UserActionManager::CommonVariables& uai) : pUAI(uai) {}
+		void UserSteppingAction(const G4Step * step);
 };
 
 class UAIUserEventAction : public G4UserEventAction {
-	UserActionsInterface * pUAI;
+	UserActionManager::CommonVariables &pUAI;
 	public:
-		UAIUserEventAction(UserActionsInterface * uai) : pUAI(uai) {}
-		void BeginOfEventAction(const G4Event * ev) {pUAI->event(ev);}
-		void EndOfEventAction(const G4Event * ev) {pUAI->eventEnd(ev);}
+		UAIUserEventAction(UserActionManager::CommonVariables& uai) : pUAI(uai) {}
+		void BeginOfEventAction(const G4Event * ev);
+		void EndOfEventAction(const G4Event * ev);
 };
 
 class UAIUserStackingAction : public G4UserStackingAction {
-	UserActionsInterface * pUAI;
+	UserActionManager::CommonVariables &pUAI;
 	public:
-		UAIUserStackingAction(UserActionsInterface * uai) : pUAI(uai) {}
-		G4ClassificationOfNewTrack ClassifyNewTrack(const G4Track* tr) {
-			return pUAI->classifyTrack(tr);
-		}
+		UAIUserStackingAction(UserActionManager::CommonVariables& uai) : pUAI(uai) {}
+		G4ClassificationOfNewTrack ClassifyNewTrack(const G4Track* tr);
 };
 
 class UAIUserTrackingAction : public G4UserTrackingAction {
-	UserActionsInterface * pUAI;
+	UserActionManager::CommonVariables &pUAI;
 	public:
-		UAIUserTrackingAction(UserActionsInterface * uai) : pUAI(uai) {}
-		void PostUserTrackingAction(const G4Track* tr) {
-			pUAI->postTracking(tr);
-		}
+		UAIUserTrackingAction(UserActionManager::CommonVariables& uai) : pUAI(uai) {}
+		void PostUserTrackingAction(const G4Track* tr);
 };
 
 // ---------------------------------------------------------------------
 //                    Histogrammer class
 // ---------------------------------------------------------------------
-UserActionManager::UserActionManager(bool store_tracks, G4String prefix) :
-	evid(-1), event_stream((prefix+"events.txt").c_str()), track_stream(store_tracks ? (prefix+"tracks.txt").c_str() : "/dev/null")
-{
-	userSteppingAction = new UAIUserSteppingAction(this);
-	userEventAction = new UAIUserEventAction(this);
-	userStackingAction = new UAIUserStackingAction(this);
-	userTrackingAction = new UAIUserTrackingAction(this);
+UserActionManager::UserActionManager(bool store_tracks, G4String prefix) {
+	pUAI.evid = -1;
+	pUAI.event_stream.open((prefix+"events.txt").c_str());
+	pUAI.track_stream.open(store_tracks ? (prefix+"tracks.txt").c_str() : "/dev/null");
 
-	hE = gsl_histogram_alloc(10);
+	userSteppingAction = new UAIUserSteppingAction(pUAI);
+	userEventAction = new UAIUserEventAction(pUAI);
+	userStackingAction = new UAIUserStackingAction(pUAI);
+	userTrackingAction = new UAIUserTrackingAction(pUAI);
+
+	pUAI.hE = gsl_histogram_alloc(10);
 	double rngs[] = {0,1e-3,1e-2,1e-1,1e0,1e1,1e2,1e3,1e4,1e5,1e6};
-	gsl_histogram_set_ranges(hE, rngs, 11);
-	for(unsigned int i=0; i<hE->n+1; i++) {
-		G4cout << i << ' ' << hE->range[i] << G4endl;
+	gsl_histogram_set_ranges(pUAI.hE, rngs, 11);
+	for(unsigned int i=0; i<pUAI.hE->n+1; i++) {
+		G4cout << i << ' ' << pUAI.hE->range[i] << G4endl;
 	}
 
 	// Write the header of the events CSV file
-	event_stream << "event,pid,name,E,x,y,z,theta,phi,px,py,pz,p_theta,p_phi" << G4endl;
-	track_stream << "event,parentid,trackid,pid,particle,Ekin" << G4endl;
+	pUAI.event_stream << "event,pid,name,E,x,y,z,theta,phi,px,py,pz,p_theta,p_phi" << G4endl;
+	pUAI.track_stream << "event,parentid,trackid,pid,particle,Ekin" << G4endl;
 }
 
 UserActionManager::~UserActionManager() {
-	track_stream.close();
-	event_stream.close();
-	gsl_histogram_free(hE);
+	pUAI.track_stream.close();
+	pUAI.event_stream.close();
+	gsl_histogram_free(pUAI.hE);
 }
 
 G4UserSteppingAction * UserActionManager::getUserSteppingAction() {
@@ -86,28 +92,28 @@ G4UserTrackingAction * UserActionManager::getUserTrackingAction() {
 	return userTrackingAction;
 }
 
-void UserActionManager::event(const G4Event * ev) {
+void UAIUserEventAction::BeginOfEventAction(const G4Event * ev) {
 	G4cout << "EVENT: " << ev->GetEventID() << G4endl;
-	evid = ev->GetEventID();
+	pUAI.evid = ev->GetEventID();
 }
 
-void UserActionManager::eventEnd(const G4Event*) {
-	evid = -1;
+void UAIUserEventAction::EndOfEventAction(const G4Event*) {
+	pUAI.evid = -1;
 }
 
-G4ClassificationOfNewTrack UserActionManager::classifyTrack(const G4Track* tr) {
-	track_stream << evid << ","
-	             << tr->GetParentID() << "," << tr->GetTrackID()
-	             << "," << tr->GetParticleDefinition()->GetPDGEncoding()
-	             << "," << tr->GetParticleDefinition()->GetParticleName()
-	             << "," << tr->GetKineticEnergy()/MeV
-	             << "," << tr->GetPosition().mag()/km
-	             << G4endl;
+G4ClassificationOfNewTrack UAIUserStackingAction::ClassifyNewTrack(const G4Track* tr) {
+	pUAI.track_stream << pUAI.evid << ","
+	                  << tr->GetParentID() << "," << tr->GetTrackID()
+	                  << "," << tr->GetParticleDefinition()->GetPDGEncoding()
+	                  << "," << tr->GetParticleDefinition()->GetParticleName()
+	                  << "," << tr->GetKineticEnergy()/MeV
+	                  << "," << tr->GetPosition().mag()/km
+	                  << G4endl;
 	return tr->GetKineticEnergy() < 20*MeV ? fKill : fUrgent;
 	//return fUrgent;
 }
 
-void UserActionManager::step(const G4Step * step) {
+void UAIUserSteppingAction::UserSteppingAction(const G4Step * step) {
 	//step->GetTrack()->GetParticleDefinition() != G4Gamma::Definition() // check gammas
 	if(step->GetPostStepPoint()->GetStepStatus() != fWorldBoundary) {
 		return;
@@ -120,36 +126,35 @@ void UserActionManager::step(const G4Step * step) {
 	G4ThreeVector pos = step->GetPostStepPoint()->GetPosition();
 	G4ThreeVector pdir = step->GetTrack()->GetMomentumDirection();
 
-
-	event_stream << std::setw(5) << evid << ',' << std::setw(4) << pid << ',' << std::setw(12) << name
-	             << std::scientific << std::setprecision(10)
-	             << ',' << E/MeV
-	             << ',' << pos.x()/km << ',' << pos.y()/km << ',' << pos.z()/km
-	             << ',' << pos.theta() << ',' << pos.phi()
-	             << ',' << pdir.x() << ',' << pdir.y() << ',' << pdir.z()
-	             << ',' << pdir.theta() << ',' << pdir.phi()
-	             << G4endl;
+	pUAI.event_stream << std::setw(5) << pUAI.evid << ',' << std::setw(4) << pid << ',' << std::setw(12) << name
+	                  << std::scientific << std::setprecision(10)
+	                  << ',' << E/MeV
+	                  << ',' << pos.x()/km << ',' << pos.y()/km << ',' << pos.z()/km
+	                  << ',' << pos.theta() << ',' << pos.phi()
+	                  << ',' << pdir.x() << ',' << pdir.y() << ',' << pdir.z()
+	                  << ',' << pdir.theta() << ',' << pdir.phi()
+	                  << G4endl;
 
 	// update the histogram
-	gsl_histogram_increment(hE, E);
+	gsl_histogram_increment(pUAI.hE, E);
 }
 
-void UserActionManager::postTracking(const G4Track* tr) {
-	track_stream << " > PostTrack(" << tr->GetTrackID() << "): stepp - " << tr->GetStep() << G4endl;
+void UAIUserTrackingAction::PostUserTrackingAction(const G4Track* tr) {
+	pUAI.track_stream << " > PostTrack(" << tr->GetTrackID() << "): stepp - " << tr->GetStep() << G4endl;
 	if(tr->GetStep()->GetPostStepPoint()->GetStepStatus() != fWorldBoundary) {
-		track_stream << "   > BOUNDARY!" << G4endl;
+		pUAI.track_stream << "   > BOUNDARY!" << G4endl;
 	}
 }
 
 void UserActionManager::saveHistograms() {
 	std::ofstream fout("histos.txt");
-	fout << hE->n << std::endl;
-	for(unsigned int i=0; i<hE->n+1; i++) {
-		fout << (i==0?"":" ") << hE->range[i];
+	fout << pUAI.hE->n << std::endl;
+	for(unsigned int i=0; i<pUAI.hE->n+1; i++) {
+		fout << (i==0?"":" ") << pUAI.hE->range[i];
 	}
 	fout << std::endl;
-	for(unsigned int i=0; i<hE->n; i++) {
-		fout << (i==0?"":" ") << hE->bin[i];
+	for(unsigned int i=0; i<pUAI.hE->n; i++) {
+		fout << (i==0?"":" ") << pUAI.hE->bin[i];
 	}
 	fout << std::endl;
 	fout.close();
--- src/UserActionManager.hh
+++ src/UserActionManager.hh
@@ -1,37 +1,16 @@
 #ifndef UserActionManager_h
 #define UserActionManager_h
 
+#include <G4String.hh>
 #include <fstream>
-
-#include <G4UserSteppingAction.hh>
-#include <G4UserEventAction.hh>
-#include <G4UserStackingAction.hh>
-#include <G4UserTrackingAction.hh>
-
-#include <G4Event.hh>
-#include <G4Step.hh>
-#include <G4Track.hh>
-
 #include <gsl/gsl_histogram.h>
 
-struct UserActionsInterface {
-	virtual void step(const G4Step * step) = 0;
-	virtual void event(const G4Event * ev) = 0;
-	virtual void eventEnd(const G4Event * ev) = 0;
-	virtual G4ClassificationOfNewTrack classifyTrack(const G4Track* tr) = 0;
-	virtual void postTracking(const G4Track* tr) = 0;
-};
+class G4UserSteppingAction;
+class G4UserEventAction;
+class G4UserStackingAction;
+class G4UserTrackingAction;
 
-class UserActionManager : private UserActionsInterface {
-	G4UserSteppingAction * userSteppingAction;
-	G4UserEventAction * userEventAction;
-	G4UserStackingAction * userStackingAction;
-	G4UserTrackingAction * userTrackingAction;
-
-	G4int evid;
-	std::ofstream event_stream, track_stream;
-	gsl_histogram * hE;
-
+class UserActionManager {
 	public:
 		UserActionManager(bool store_tracks, G4String prefix = "");
 		~UserActionManager();
@@ -43,12 +22,19 @@ class UserActionManager : private UserActionsInterface {
 		G4UserStackingAction * getUserStackingAction();
 		G4UserTrackingAction * getUserTrackingAction();
 
+		struct CommonVariables {
+			G4int evid;
+			gsl_histogram * hE;
+			std::ofstream event_stream;
+			std::ofstream track_stream;
+		};
+
 	private:
-		void step(const G4Step * step);
-		void event(const G4Event * ev);
-		void eventEnd(const G4Event * ev);
-		G4ClassificationOfNewTrack classifyTrack(const G4Track* tr);
-		void postTracking(const G4Track* tr);
+		G4UserSteppingAction * userSteppingAction;
+		G4UserEventAction * userEventAction;
+		G4UserStackingAction * userStackingAction;
+		G4UserTrackingAction * userTrackingAction;
+		CommonVariables pUAI;
 };
 
 #endif
