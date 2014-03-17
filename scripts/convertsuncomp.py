5e3d20c1391573edc72be584714e3838430f44e9
Morten Piibeleht <morten.piibeleht@cern.ch>
2014-03-17 17:11:51 +0200

    Update and refactor the conversion script.

--- scripts/convertsuncomp.py
+++ scripts/convertsuncomp.py
@@ -1,40 +1,16 @@
 #!/usr/bin/env python
+"""
+Converts a .dat file of the solar composition to a YAML file.
+"""
 import re
 import yaml
+from datetime import datetime
 
-ifile = 'sun_composition.dat'
-ofile = 'solarmodel.yml'
+# Configuration variables
+cfg_ifname = 'sun_composition.dat'
+cfg_ofname = 'solarmodel.yml'
 
-# Read the .dat file
-print 'Open input file:', ifile
-fin=open(ifile)
-rxp=re.compile('([A-Za-z0-9_]+)\(([A-Za-z0-9/\-]+)\)|([A-Za-z0-9_]+)')
-cat_matches = rxp.findall(fin.readline())+rxp.findall(fin.readline())+rxp.findall(fin.readline())
-cats = [c3 if len(c1)==0 else c1 for  c1,c2,c3 in cat_matches]
-
-lys=[]
-for ln in fin.readlines():
-	ln = ln.strip()
-	if len(ln)==0 or ln[0] == '#':
-		continue
-	ps=ln.split()
-	ps=[int(ps[0])]+[float(s) for s in ps[1:]]
-	lys.append(dict(zip(cats, ps)))
-
-# Define a few convenience functions
-def prev_next_rowiterator(iterator):
-	prv = None
-	cur = iterator.next()
-	nxt = iterator.next()
-	try:
-		while True:
-			yield (prv,cur,nxt)
-			prv = cur
-			cur = nxt
-			nxt = iterator.next()
-	except StopIteration:
-		yield (prv,cur,None)
-
+# Convenience functions
 def component(e, n):
 	return {'element': e, 'number_density': float(n)}
 
@@ -44,64 +20,91 @@ def isotopes(e, isos):
 		isotopes.append({'A':i[0], 'number_density': float(i[1])})
 	return {'element': e, 'isotopes': isotopes}
 
-# Start constructing the YAML model
-mdlyaml = {
-	'name': 'The Sun (Feb 24)',
-}
-mdlyaml_layers = {'layers': []}
+if __name__=='__main__':
+	# Read the .dat file
+	print 'Opening the input file:', cfg_ifname
+	fin=open(cfg_ifname)
 
-r_prv = 0.0
-for prv,cur,nxt in prev_next_rowiterator(iter(lys)):
-	if nxt is not None:
-		r = 0.5*(nxt['radius'] + cur['radius'])
-	else:
-		r = 2*cur['radius'] - r_prv
-	ly = {
-		'thickness': float(r-r_prv),
-		'temperature': float(cur['Temp']),
-		'pressure': float(0.1 * cur['pres']), # from dyne/cm2 -> Pa
-		'components': []
+	# The first three lines are assumed to be the column headers
+	column_matches = []
+	rxp=re.compile('([A-Za-z0-9_]+)\(([A-Za-z0-9/\-]+)\)|([A-Za-z0-9_]+)')
+	for _ in range(3):
+		column_matches += rxp.findall(fin.readline())
+	columns = [c3 if len(c1)==0 else c1 for c1,c2,c3 in column_matches]
+	#print 'Columns:', columns
+
+	# The rest of the lines are either empty or space-delimited rows of values
+	layers = []
+	for line in fin.readlines():
+		line = line.strip()
+		if len(line)==0 or line[0] == '#':
+			continue
+		valuestrings=line.split()
+		values =[int(valuestrings[0])] # first value is the index (i.e. int)
+		values+=[float(s) for s in valuestrings[1:]] # all others are floats
+		layers.append(dict(zip(columns, values)))
+
+	# Start constructing the YAML model
+	print 'Constructing the dictionary'
+	model = {
+		'name': 'The Sun',
+		'date': 'Feb 24',
+		'convert_datetime': str(datetime.now())
 	}
+	model_layers = {'layers': []}
 
-	ly['components'].append(component('H', cur['n_H']))
+	last_radius = 0.0
+	for layer in layers:
+		r = layer['radius'] - last_radius
 
-	ly['components'].append(component('Ne', cur['n_Ne']))
-	ly['components'].append(component('Na', cur['n_Na']))
-	ly['components'].append(component('Mg', cur['nMg']))
-	ly['components'].append(component('Al', cur['n_Al']))
-	ly['components'].append(component('Si', cur['n_Si']))
-	ly['components'].append(component('P', cur['n_P']))
+		ly = {
+			'thickness': float(r),
+			'temperature': float(layer['Temp']),
+			'pressure': float(0.1 * layer['pres']), # from dyne/cm2 -> Pa
+			'components': []
+		}
 
-	ly['components'].append(component('S', cur['n_S']))
-	ly['components'].append(component('Cl', cur['n_Cl']))
-	ly['components'].append(component('Ar', cur['n_Ar']))
-	ly['components'].append(component('K', cur['n_K']))
-	ly['components'].append(component('Ca', cur['n_Ca']))
-	ly['components'].append(component('Sc', cur['n_Sc']))
+		ly['components'].append(component('H', layer['n_H']))
 
-	ly['components'].append(component('Ti', cur['n_Ti']))
-	ly['components'].append(component('V', cur['n_V']))
-	ly['components'].append(component('Co', cur['n_Co']))
-	ly['components'].append(component('Ni', cur['n_Ni']))
-	ly['components'].append(component('Cr', cur['n_Cr']))
-	ly['components'].append(component('Mn', cur['n_Mn']))
-	ly['components'].append(component('Fe', cur['n_Fe']))
+		ly['components'].append(component('Ne', layer['n_Ne']))
+		ly['components'].append(component('Na', layer['n_Na']))
+		ly['components'].append(component('Mg', layer['nMg']))
+		ly['components'].append(component('Al', layer['n_Al']))
+		ly['components'].append(component('Si', layer['n_Si']))
+		ly['components'].append(component('P', layer['n_P']))
 
-	ly['components'].append(isotopes('He', [(3, cur['n_He3']), (4, cur['n_He4'])]))
-	ly['components'].append(isotopes('C', [(12, cur['n_C12']), (13, cur['n_C13'])]))
-	ly['components'].append(isotopes('N', [(14, cur['n_N14']), (15, cur['n_N15'])]))
-	ly['components'].append(isotopes('O', [(16, cur['n_o16']), (17, cur['n_o17']), (18, cur['n_o18'])]))
+		ly['components'].append(component('S', layer['n_S']))
+		ly['components'].append(component('Cl', layer['n_Cl']))
+		ly['components'].append(component('Ar', layer['n_Ar']))
+		ly['components'].append(component('K', layer['n_K']))
+		ly['components'].append(component('Ca', layer['n_Ca']))
+		ly['components'].append(component('Sc', layer['n_Sc']))
 
-	mdlyaml_layers['layers'].append(ly)
-	r_prv = r
+		ly['components'].append(component('Ti', layer['n_Ti']))
+		ly['components'].append(component('V', layer['n_V']))
+		ly['components'].append(component('Co', layer['n_Co']))
+		ly['components'].append(component('Ni', layer['n_Ni']))
+		ly['components'].append(component('Cr', layer['n_Cr']))
+		ly['components'].append(component('Mn', layer['n_Mn']))
+		ly['components'].append(component('Fe', layer['n_Fe']))
 
-print 'Write the model to the output file:', ofile
-oyaml  = "# Solar atmosphere\n"
-oyaml += yaml.dump(mdlyaml, default_flow_style=False)
-oyaml += "\n"
-oyaml += yaml.dump(mdlyaml_layers)
-fout = open('solarmodel.yml', 'w')
-fout.write(oyaml)
-fout.close()
+		ly['components'].append(isotopes('He', [(3, layer['n_He3']), (4, layer['n_He4'])]))
+		ly['components'].append(isotopes('C', [(12, layer['n_C12']), (13, layer['n_C13'])]))
+		ly['components'].append(isotopes('N', [(14, layer['n_N14']), (15, layer['n_N15'])]))
+		ly['components'].append(isotopes('O', [(16, layer['n_o16']), (17, layer['n_o17']), (18, layer['n_o18'])]))
 
-print 'All done!'
+		model_layers['layers'].append(ly)
+		last_radius = r
+
+	print 'Dumping the model into YAML'
+	oyaml  = "# Solar atmosphere\n"
+	oyaml += yaml.dump(model, default_flow_style=False)
+	oyaml += "\n"
+	oyaml += yaml.dump(model_layers)
+
+	print 'Writing the model to the output file:', cfg_ofname
+	fout = open(cfg_ofname, 'w')
+	fout.write(oyaml)
+	fout.close()
+
+	print 'All done!'
