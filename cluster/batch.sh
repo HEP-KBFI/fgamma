5331483801952921914743306b506209f922c91c
Morten Piibeleht <morten.piibeleht@cern.ch>
2014-03-26 10:46:12 +0200

    Update the cluster script.

--- cluster/batch.sh
+++ cluster/batch.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 #SBATCH -J "fgamma"
+echo "@ FGAMMA START" `date`
+
+echo "@ SLURM_JOB_ID" $SLURM_JOB_ID
+echo "@ SLURM_JOB_NAME" $SLURM_JOB_NAME
+echo "@ SLURM_JOB_NUM_NODES" $SLURM_JOB_NUM_NODES
+echo "@ SLURM_JOB_NODELIST" $SLURM_JOB_NODELIST
+
 seed=$(expr `date +%s` + $RANDOM)
-time ./fgamma --seed=${seed} $@
+
+set -x
+time ./fgamma -v0 --seed=${seed} $@
+{ set +x; } 2>/dev/null
+
+echo "@ FGAMMA END" `date`
